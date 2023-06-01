6a9b7f1a9a8d2672f5b05af4e2eacfaf379249c4
Filip Slanina <10fili10@seznam.cz>
2023-06-01 16:16:24 +0200

    Workout 04 for EDNA

---
+++ Workout 04/work04.py
@@ -0,0 +1,28 @@
+import math
+
+"""
+Problem 1: Create a function that takes an angle in radians
+and returns the corresponding angle in degrees rounded
+to one decimal place.
+"""
+
+
+def radians_to_degrees(value: float) -> float:
+    return round(value * 180 / math.pi, 1)
+
+
+print(radians_to_degrees(20))
+
+
+"""
+Problem 2: Create a function that returns True
+when num1 is equal to num2; otherwise return False
+"""
+
+
+def is_same_num(a, b):
+    return a == b
+
+
+print(is_same_num(4, 8))
+print(is_same_num(2, 2))
