5d4b0bfda00238dd4b4962a1bab1c366eb9e0f24
Filip Slanina <10fili10@seznam.cz>
2023-06-01 17:53:00 +0200

    Added non-negativ ints

--- Workout 06/work06.py
+++ Workout 06/work06.py
@@ -6,9 +6,9 @@ a new list without the strings.
 
 
 def filter_list(value: list) -> list:
-    return [i for i in value if type(i) == int]
+    return [i for i in value if type(i) == int and i >= 0]
 
 
-print(filter_list([1, 2, "a", "b"]))
+print(filter_list([1, -2, "a", "b"]))
 print(filter_list([1, "a", "b", 0, 15]))
 print(filter_list([1, 2, "aasf", "1", "123", 123]))
