752bfbb9ccca6bafdb4a6da039bab4f83a9bd6b7
Filip Slanina <10fili10@seznam.cz>
2023-06-01 17:51:21 +0200

    Python Workout 06 solution EDNA

---
+++ Workout 06/work06.py
@@ -0,0 +1,14 @@
+""" 
+Problem: Create a function that takes a list
+of non-negative integers and strings and return
+a new list without the strings.
+"""
+
+
+def filter_list(value: list) -> list:
+    return [i for i in value if type(i) == int]
+
+
+print(filter_list([1, 2, "a", "b"]))
+print(filter_list([1, "a", "b", 0, 15]))
+print(filter_list([1, 2, "aasf", "1", "123", 123]))
