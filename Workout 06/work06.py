bb3f986485b9ff8d65eda31e9ed1c85fee5cd0c1
Filip Slanina <10fili10@seznam.cz>
2023-06-01 17:58:53 +0200

    Added examples

--- Workout 06/work06.py
+++ Workout 06/work06.py
@@ -9,6 +9,6 @@ def filter_list(value: list) -> list:
     return [i for i in value if type(i) == int and i >= 0]
 
 
-print(filter_list([1, -2, "a", "b"]))
+print(filter_list([1, 2, "a", "b"]))
 print(filter_list([1, "a", "b", 0, 15]))
 print(filter_list([1, 2, "aasf", "1", "123", 123]))
